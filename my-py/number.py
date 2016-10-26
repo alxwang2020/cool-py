6e1f785ee9a8d61599ea636cecd681c855d83afa
alxwang2020 <you@example.com>
2016-10-26 15:18:34 +0800

    add my-py

---
+++ my-py/number.py
@@ -0,0 +1,14 @@
+a = float (raw_input("print a:"))
+b = float (raw_input("print b:"))
+c = "%.2f"%(a + b)
+d = "%.2f"%(a * b)
+e = "%.2f"%(a - b)
+f = "%.2f"%(a / b)
+g = "%.2f"%(a % b)
+print "a + b =  ",c
+print "a * b =  ",d
+print "a - b =  ",e
+print "a / b =  ",f
+print "a % b =  ",g
+
+
