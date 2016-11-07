97cf72f3fdc229003156a08ff4baf6360947348c
alxwang2020 <you@example.com>
2016-11-07 14:57:44 +0800

    spider

---
+++ jpgspider.py
@@ -0,0 +1,18 @@
+import re
+import urllib
+
+def getHTML(url):
+	page = urllib.urlopen(url)
+	html = page.read()
+	return html
+def geturl(html):
+    r= r'src="(.*?\.jpg)"'
+    s = re.compile(r)
+    fhtml = re.findall(s,html)
+    i = 0
+    for x in fhtml:
+    	urllib.urlretrieve(x,'%s.jpg'%i)
+        i= i+1
+    	print i,'.jpg'
+html = getHTML("http://www.mm131.com")
+geturl(html)
