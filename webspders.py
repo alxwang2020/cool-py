4fe20ef50e60dab7ab5980048c52e8d2bcd4f176
alxwang2020 <you@example.com>
2016-12-06 17:44:09 +0800

    sorry

---
+++ webspders.py
@@ -0,0 +1,20 @@
+import re
+import urllib
+
+def getHTML(url):
+	page = urllib.urlopen(url)
+	html = page.read()
+	return html
+def geturl(html):
+    r= r'src="(.*?\.jpg)"'
+    s = re.compile(r)
+    fhtml = re.findall(s,html)
+    i = 0
+    for x in fhtml:
+    	urllib.urlretrieve(x,'%s.jpg'%i)
+    	i= i+1
+    	print i,".jpg"
+    	print "done"
+url =input("plz type your website:")
+html = getHTML(url)
+geturl(html)
